a8440805d181475bee6e72c22ce4fc585fb0d286
Ejiro-David <daviderhabor.de@gmail.com>
2024-06-27 06:44:41 -0700

    updated done challenges

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (typeof Number(process.argv[2]) !== 'number') {
+if (isNaN(process.argv[2])) {
 	console.log('Not a number');
 } else {
 	console.log('My number: ' + process.argv[2]);
